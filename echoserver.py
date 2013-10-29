95868c58722abd32d41923bfc1164a425d120c20
Fei Dong <feinomenon@gmail.com>
2013-10-29 17:28:02 -0400

    Interpret the first message from a client as its username and send each subsequent message to every other client.

--- echoserver.py
+++ echoserver.py
@@ -1,25 +1,47 @@
-from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
-# from twisted.words.protocols import irc
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet.task import LoopingCall
+from twisted.protocols.basic import LineReceiver
 
-# import sys
-
-class EchoServer(Protocol):
-    def dataReceived(self, data):
-        print "Received msg: %s" % (data)
-        self.transport.write("Client said: " + data)
+class ChatServer(LineReceiver):
+    def __init__(self, factory, addr):
+        self.factory = factory
+        self.addr = addr
+        self.username = None
 
     def connectionMade(self):
-        self.transport.write("[Insert swag here]")
+        # self.peer = self.transport.getPeer()
+        print "Current users: "
+        self.sendLine("What is your name?")
 
-# def confirm():
-#     print "Reactor running."
+    def lineReceived(self, line):
+        print "Received msg: %s" % (line)
+        if not self.username:
+            self.username = line
+            self.factory.clients[self] = self.username
+            self.sendLine("Hey " + self.username)
+        else:
+            for client in self.factory.clients:
+                if client != self:
+                    client.sendLine(line)
+
+    def connectionLost(self, reason):
+        del self.factory.clients[self]
+        for client in self.factory.clients:
+            client.sendLine("%s has left." % (self.addr))
+
+class ChatFactory(Factory):
+    def __init__(self):
+        self.clients = dict()
+
+    def buildProtocol(self, addr):
+        print "%s wants to connect!" % (addr.host)
+        return ChatServer(self, addr)
+
 
 def main():
-    # reactor.callWhenRunning(confirm)
-    f = Factory()
-    f.protocol = EchoServer
-    reactor.listenTCP(8000, f)
+    reactor.listenTCP(8000, ChatFactory())
+    print "Listening on port 8000..."
     reactor.run()
 
 if __name__ == "__main__":
